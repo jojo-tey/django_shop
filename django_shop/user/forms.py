ead62ab7749d580f884d800fa276e11cefb0c241
jojo-tey <dosagam6301@gmail.com>
2021-01-28 16:03:40 +0100

    #3 User register, login
    Used form view

--- django_shop/user/forms.py
+++ django_shop/user/forms.py
@@ -1,24 +1,53 @@
 from django import forms
+from .models import User
+from django.contrib.auth.hashers import check_password, make_password
 
 
 class RegisterForm(forms.Form):
-    email = forms.EmailField(max_length=64, label='Email')
+    email = forms.EmailField(error_messages={
+        'required': 'Please enter your email'}, max_length=64, label="EMAIL")
+    password = forms.CharField(error_messages={
+        'required': 'Please enter your password'}, widget=forms.PasswordInput, label="PASSWORD")
+    con_password = forms.CharField(error_messages={
+        'required': 'Please enter your password again'}, widget=forms.PasswordInput, label="PASSWORD CONFIRM")
 
-    password = forms.CharField(widget=forms.PasswordInput, label='Password')
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        con_password = cleaned_data.get('con_password')
 
-    re_password = forms.CharField(
-        widget=forms.PasswordInput, label='Re_password')
+        if password and con_password:
+            if password != con_password:
+                self.add_error('password', 'Password is not matched')
+                self.add_error('con_password', 'Password is not matched')
+            else:
+                user = User(
+                    email=email,
+                    password=make_password(password)
+                )
+                user.save()
 
 
-# error_messages={
-#             'required': 'Enter your email address'
-#         },
+class LoginForm(forms.Form):
+    email = forms.EmailField(error_messages={
+        'required': 'Please enter your email'}, max_length=64, label="EMAIL")
+    password = forms.CharField(error_messages={
+        'required': 'Please enter your password'}, widget=forms.PasswordInput, label="PASSWORD")
 
-#         error_message={
-#         'required': 'Enter your password'
-#     },
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
 
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                self.add_error('Email', 'This email is not registered')
+                return
 
-#     error_message={
-#         'required': 'Enter your password again'
-#     },
+            if not check_password(password, user.password):
+                self.add_error('password', 'Wrong password')
+            else:
+                self.email = user.email
