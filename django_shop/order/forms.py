d2717c224b7133cabcfe1c24de261477985ca876
jojo-tey <dosagam6301@gmail.com>
2021-01-29 16:23:02 +0100

    #5 Order create, Ordered list

---
+++ django_shop/order/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Order
+from product.models import Product
+from user.models import User
+
+
+class RegisterForm(forms.Form):
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
+    quantity = forms.IntegerField(error_messages={
+        'required': 'Please enter quantity'
+    }, label="Quantity")
+
+    product = forms.IntegerField(error_messages={
+        'required': 'Please write product description'}, label='Product description', widget=forms.HiddenInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        quantity = cleaned_data.get('quantity')
+        product = cleaned_data.get('product')
+        user = self.request.session.get('user')
+
+        if quantity and product and user:
+            order = Order(
+                quantity=quantity,
+                product=Product.objects.get(pk=product),
+                user=User.objects.get(email=user)
+            )
+            order.save()
+        else:
+            self.product = Product
+            self.add_error('quantity', 'No result')
+            self.add_error('product', 'No result')
