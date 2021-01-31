ffc8e4e0a4020fc90659670de251ce471dcd17d1
jojo-tey <dosagam6301@gmail.com>
2021-01-31 12:46:47 +0100

    #8 RestAPI
    Used Django REST framework

--- django_shop/django_shop/settings.py
+++ django_shop/django_shop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'rest_framework',
     'user',
     'order',
     'product',
